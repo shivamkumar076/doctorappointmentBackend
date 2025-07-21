15760e234dbc6fc4deb3e41ec76f75c6a3e3b0ea
Shivam Kumar <shivamkumar776499@gmail.com>
2025-07-21 16:01:46 +0530

    add url

--- src/app.js
+++ src/app.js
@@ -12,7 +12,21 @@ app.use(cors({
     
     credentials: true,
 }));
+const allowedOrigins = [
+  "https://doctorappointment-frontend-three.vercel.app", // Production
+  "http://localhost:5175" // Development
+];
 
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 app.use(cookieParser());
